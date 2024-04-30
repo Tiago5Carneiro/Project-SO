ca5f372d520acba99a50f41113694f11bce0bedc
Tiago <tiago.carneiro.email@gmail.com>
2024-04-30 20:37:40 +0100

    Commit

--- src/client.c
+++ src/client.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>  /* O_RDONLY, O_WRONLY, O_CREAT, O_APPEND, O_RDWR , O_TRUNC, O_EXCL, O_* */
 #include <stdio.h>  /* printf */
 #include <signal.h> /* signals */
+#include <sys/stat.h> 	/* mkfifo */
 #include <stdbool.h>
 #include <string.h> 
 
@@ -9,27 +10,110 @@
 char buff[BUFF_SIZE] ;
 
 int server_fifo,
-	client_fifo;
+	read_fifo,
+	write_fifo;
 
-void handle_usr1(){
+// Funcao para dar flip a uma string
+void flip(char* string){
+    int i,j;
+    
+    char aux;
 
-	printf("Done executing\n");
+    for (i = 0, j = strlen(string) - 1 ; i < j; i++,j--)
+    {
+        aux = string[i];
+        string[i] = string[j];
+        string[j] = aux;
+    }
+}
 
+// Definir itoa para poder usar quando sao criados os fifos de cada cliente
+void itoa(int n, char* string){
+    int i, sign;
+
+    // Verificar o sinal 
+    if ((sign = n) < 0)n = -n;
+
+    i = 0;
+
+    // Gerar os digitos na ordem inversa
+    do
+    {                  
+    	// Prox digito       
+        string[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0); // Eliminar digito
+    
+
+    if (sign < 0)
+        string[i++] = '-';
+    string[i] = '\0';
+
+    flip(string);
+}
+
+// Funcao para lidar com a terminacao do cliente
+void handle_signalterm(){
+	printf("Task done...\n");
+	
+	// Eliminar fifos
+	char client_fifo[26];
+	int pid = getpid();
+	char pid_name[19];
+	itoa(pid,pid_name);
+
+	strcpy(client_fifo,"tmp/w_");
+	strcpy(client_fifo+7,pid_name);
+
+	unlink(client_fifo);
+
+	strcpy(client_fifo,"tmp/r_");
+	strcpy(client_fifo+7,pid_name);
+
+	unlink(client_fifo);
+	
+	_exit(0);
+}
+
+// Funcao para lidar com Ctrl + C
+void handle_signalint(){
+
+	printf("\n\nUNEXPECTED TERMINATION \n\n");
+
+	// Eliminar fifos
+	char client_fifo[26];
+	int pid = getpid();
+	char pid_name[19];
+	itoa(pid,pid_name);
+
+	strcpy(client_fifo,"tmp/w_");
+	strcpy(client_fifo+7,pid_name);
+
+	unlink(client_fifo);
+
+	strcpy(client_fifo,"tmp/r_");
+	strcpy(client_fifo+7,pid_name);
+
+	unlink(client_fifo);
+
+	// Colocar o handler para o sinal int (Ctrl + C) de volta para a funcao default do kernel (SIG_DFL)
+	signal(SIGINT,SIG_DFL);
+
+	// Mandar um singal sig int para si novamente 
+	kill(getpid(),SIGINT);
 }
 
 // Funcao para escrever o status para o fifo do cliente 
-int status(int server_fifo,int client_fifo){
+int send_status(){
 
 	printf("Getting status ...\n");
 
 	// Escrever status
-	write(client_fifo,"status",sizeof(char)*6);
-
+	write(write_fifo,"status",sizeof(char)*6);
 
 	// Ficar a espera da resposta de volta do servidor que ele vai mandar pelo seu fifo
-	read(server_fifo,buff,BUFF_SIZE);
+	read(read_fifo,buff,BUFF_SIZE);
 
-	printf("%s\n",buff);
+	printf("Message : %s\n",buff);
 	return 0;
 } 
 
@@ -39,31 +123,59 @@ int execute(int client_fifo, char* command){
 	printf("Executing ...\n");
 	
 	// Escrever no fifo para o servidor receber
-	write(client_fifo,command,sizeof(command));
-
+	write(write_fifo,command,sizeof(command));
 
 	// Esperar pela resposta do servidor que vai ser mandada pelo seu fifo
 	read(server_fifo,buff,BUFF_SIZE);
 
 	printf("%s\n",buff);
 
-	// Mandar sinal USR1 para si mesmo
-	kill(getpid(),SIGUSR1);
-
 	return 0;
 }
 
 int main(int argc, char* argv[]){
 
-	// Abertura dos fifos 
-	client_fifo = open("./tmp/client_fifo", O_WRONLY);
-	server_fifo = open("./tmp/server_fifo", O_RDONLY);
+	signal(SIGINT,handle_signalint);
+	signal(SIGTERM,handle_signalterm);
 
-	// Definir a funcao que da handle ao sinal SIGUSR1
-	signal(SIGUSR1, handle_usr1);
+	// Abertura dos fifos 
+	server_fifo = open("./tmp/server_fifo", O_WRONLY);
+
+	// Colucar o pid numa string para depois usar para criar os seus fifos respetivos
+	int pid = getpid();
+	char pid_name[19];
+	itoa(pid,pid_name);
+
+	if (write(server_fifo,pid_name,sizeof(pid_t))==-1){
+		perror("Write Pid Fifo"); 
+	}
+
+	// String que vai conter o path e nome do fifo de escrita do cliente
+	char client_fifo[26];
+
+	strcpy(client_fifo,"tmp/w_");
+	strcpy(client_fifo+7,pid_name);
+
+
+	if(mkfifo(client_fifo,0666)==-1){
+		perror("Criacao fifo escrita");
+	}
+
+	write_fifo = open(client_fifo,O_WRONLY);
+
+	strcpy(client_fifo,"tmp/r_");
+	strcpy(client_fifo+7,pid_name);
+
+
+	if(mkfifo(client_fifo,0666)==-1){
+		perror("Criacao fifo leitura");
+	}
+
+	read_fifo = open(client_fifo,O_RDONLY);
 
 	bool type=0;
 
+	/*
 	// Verificar se e um execute ou um status
 	(strcmp(argv[1],"execute")==0) ? type = 1 : ((strcmp(argv[1],"status")==0) ? type = 0 : perror("Arguments"));
 
@@ -78,7 +190,6 @@ int main(int argc, char* argv[]){
     	//switch para o mode selecionado
         switch (mode) {    
             case 'p':
-                execute(client_fifo,argv[3]);
                 break;
     
             default:
@@ -88,7 +199,23 @@ int main(int argc, char* argv[]){
     }
     else{
     	// Status
-    	status(server_fifo,client_fifo);
+    	send_status();
     }    
+	
+	*/
+
+	printf("Hello\n");
+	send_status();
+    strcpy(client_fifo,"tmp/w_");
+	strcpy(client_fifo+7,pid_name);
+
+	unlink(client_fifo);
+
+	strcpy(client_fifo,"tmp/r_");
+	strcpy(client_fifo+7,pid_name);
+
+	unlink(client_fifo);
+
+
 	return 0;
 }
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -6,7 +6,47 @@
 #include <sys/stat.h> 	/* mkfifo */
 #include <sys/wait.h> 	/* wait */
 #include <sys/types.h>
+#include <string.h>
 
+/*
+
+	Ideas on how to do things : 
+
+	Cliente vai mandar a string toda que quer executar 
+
+	Server verifica se recebeu status ou exectute
+
+		Se status verificar logo como esta a queue e mandar a informacao para o cliente, depois mandar 
+		a informacao da queue que contem os commandos que estao a ser executados agora 
+			Uma ideia seria mandar strings individuais por processo em espera e por processo a ser executado 
+			Parar de mandar atravez de um character especifico e assim que o cliente receber este character 
+			parar de ler do fifo
+
+		Se execute for recebido 
+			verificar se foi usado p ou u
+				se u :
+					fazer um filho que vai usar o execvp para executar o commando dado 
+					direcionar o stdout para um ficheiro no path dado como argumento ao servidor 
+				se p :
+					usar o strstep para encontrar o caracter '|' e por cada vez que encontrar ou quando encontrar \0, 
+					criar filho para executar cada um destes commandos 
+					cada filho direciona o stdout para um ficheiro que ira para o path 
+
+		Ideia de como gerir a queue 
+			Processo pai do programa vai ser o processo que esta sempre a ler do fifo 
+			Este cria um filho logo no inicio, que vai ser quem gere a queue 
+			Este filho vai ficar a ler dum pipe vindo do pai ate receber informacao para criar um processo para a queue 
+			Quando ler um novo pedido vai verificar na queue de processos a serem processados neste momento se pode ser 
+			ja iniciado este pedido 
+			Se sim cria logo um filho para comecar a tratar desse processo e adiciona o processo a queue dos que estao 
+			a ser executados
+			Otherwise vai para a queue dos de espera
+			
+		Como fazer ir da queue de espera para ser processado
+			Quando o filho terminar de processar entao de alguma maneira um processo deveria verificar se o proximo pro
+			cesso da queue ja pode ser feito 
+
+*/
 
 #define BUFFER_SIZE 1024
 
@@ -14,24 +54,64 @@ static volatile int is_open = 1;
 char* output_path;
 char buff[BUFFER_SIZE];
 
+pid_t pids[50];
+
 // ./orchestrator {output_folder} {parallel-tasks} {sched-policy}
 
+// Funcao para dar flip a uma string
+void flip(char* string){
+    int i,j;
+    
+    char aux;
+
+    for (i = 0, j = strlen(string) - 1 ; i < j; i++,j--)
+    {
+        aux = string[i];
+        string[i] = string[j];
+        string[j] = aux;
+    }
+}
+
+// Definir itoa para poder usar quando sao criados os fifos de cada cliente
+void itoa(int n, char* string){
+    int i, sign;
+
+    // Verificar o sinal 
+    if ((sign = n) < 0)n = -n;
+
+    i = 0;
+
+    // Gerar os digitos na ordem inversa
+    do
+    {                  
+    	// Prox digito       
+        string[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0); // Eliminar digito
+    
+
+    if (sign < 0)
+        string[i++] = '-';
+    string[i] = '\0';
+
+    flip(string);
+}
+
+// Funcao para lidar com o fecho do servidor
 void handle_signalterm(){
 	printf("Closing server...\n");
 	
 	// Eliminar fifos
-	unlink("./tmp/client_fifo");
 	unlink("./tmp/server_fifo");
 	
 	_exit(0);
 }
 
+// Funcao para lidar com Ctrl + C
 void handle_signalint(){
 
 	printf("\n\nUNEXPECTED TERMINATION \n\n");
 
 	// Eliminar fifos
-	unlink("./tmp/client_fifo");
 	unlink("./tmp/server_fifo");
 
 	// Colocar o handler para o sinal int (Ctrl + C) de volta para a funcao default do kernel (SIG_DFL)
@@ -41,9 +121,6 @@ void handle_signalint(){
 	kill(getpid(),SIGINT);
 }
 
-int read_child(){
-
-}
 
 int main(int argc, char* argv[]){
 
@@ -63,42 +140,66 @@ int main(int argc, char* argv[]){
 	 	perror("Fifo server");
 	 	return -1;
 	 }
- 	if(mkfifo("./tmp/client_fifo",0644)==-1){
-	 	perror("Fifo server");
-	 	return -1;
-	 }
 
 	// Indicar a funcao que vai tratar do sinal sigterm
 	signal(SIGTERM,handle_signalterm);
 	signal(SIGINT,handle_signalint);
 
 	// // Descritores para os fifos
-	int client_fd = open("./tmp/client_fifo",O_RDONLY);
-	int server_fd = open("./tmp/server_fifo",O_WRONLY);
+	int server_fd = open("./tmp/server_fifo",O_RDONLY);
+	
+	pid_t pid;
+	int i;
 	int status;
 	while(is_open){
 		
 		// Ler fifo para onde o cliente escreve
-		read(client_fd,buff,BUFFER_SIZE);
+		read(server_fd,&pid,sizeof(pid_t));
+		i = 0;
+		while(pids[i]!=0){ 
+			i++;
+		}
+		pids[i] = pid;
 		if(fork()==0){
 			
 			// filho que vai tratar da informacao providenciada pelo cliente
-			printf("%s\n",buff);
+			
+			// Eliminar fifos
+			char client_fifo[26];
+			char pid_name[19];
+			itoa(pid,pid_name);
+
+			strcpy(client_fifo,"tmp/w_");
+			strcpy(client_fifo+7,pid_name);
+
+			int read_client_fifo = open(client_fifo,O_RDONLY);
+
+			strcpy(client_fifo,"tmp/r_");
+			strcpy(client_fifo+7,pid_name);
+
+			int write_client_fifo = open(client_fifo,O_WRONLY);
+
+			read(read_client_fifo,buff,BUFFER_SIZE);
+			
+			printf("Mensagem : %s\n",buff);
 
 			// filho informa cliente que o seu pedido ja foi concluido
-			write(server_fd,"done",sizeof(char)*4);
+			write(write_client_fifo,"Done",sizeof(char)*4);
+
+			close(write_client_fifo);
+			close(read_client_fifo);
 
 			// filho termina 
 			_exit(0);
 
 		}
+		wait(&status);
+		is_open = 0;
 	}
 	// Fechar descritor fifos
-	close(client_fd);
 	close(server_fd);
 		
 	// Eliminar fifos
-	unlink("./tmp/client_fifo");
 	unlink("./tmp/server_fifo");
 	
 	return 0;
